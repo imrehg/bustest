f8a99d6fce5ef1bc8207786a41f95bcc7fba684f
Gergely Imreh <imrehg@gmail.com>
2013-10-18 12:50:51 +0800

    better database connection

--- web.js
+++ web.js
@@ -25,23 +25,38 @@ settings.total = {"endpoint": settings.endpoint, "buses": settings.buses};
 var tracking_table = process.env.MYSQL_TABLE
   , tokens = process.env.TOKENS.split(",")
 ;
-
-// Set up database connection
-var connection = mysql.createConnection({
+var db_config = {
   host     : process.env.MYSQL_HOST,
   user     : process.env.MYSQL_USER,
   password : process.env.MYSQL_PASS,
   database : process.env.MYSQL_DB
-});
-connection.connect(function(err) {
-  if (err) {
-      console.log(err);
-      console.log("No connection, data won't be saved!");
-  } else {
-      console.log("Connected to backend database.");
-  }
-});
+};
 
+// Set up database connection
+var connection;
+
+// Handle disconnects, from https://github.com/felixge/node-mysql
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+handleDisconnect();
 
 var app = express()
   , http = require('http')
